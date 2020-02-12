5b700af78b61965d7dab4a86fc80f07c3dcd9a64
nguiyim <jnguiyim17@students.desu.edu>
2020-02-12 18:33:55 -0500

    initial commit

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Jessica Nguiyim on 2/10/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath)
+        return cell
+    }
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+}
