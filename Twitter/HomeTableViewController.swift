f2b64179383a365a7377365b50a3824192bd039f
nguiyim <jnguiyim17@students.desu.edu>
2020-02-12 19:41:55 -0500

    Finished required stories

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,28 +18,28 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
+        loadTweets()
         //where is this happening, what do you want us to do
-        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
     
     //function that calls the api
-    @objc func loadTweet(){
+    @objc func loadTweets(){
         
+        numberOfTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 20]
+        let myParams = ["count": numberOfTweet]
         //pull the tweets call the api
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             //clean up
             self.tweetArray.removeAll()
             for tweet in tweets {
-                
                 self.tweetArray.append(tweet)
             }
             
-        //always make sure any time you make a call to the api
-        //repopulate list reload new
+            //always make sure any time you make a call to the api
+            //repopulate list reload new
             self.tableView.reloadData()
             
             //end refreshing button
@@ -51,12 +51,34 @@ class HomeTableViewController: UITableViewController {
         })
         
     }
+ 
+    func loadMoreTweets(){
+        let myUrl  = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweet = numberOfTweet + 20
+        let myParams = ["count": numberOfTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            //clean up
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets! oh no")
+        })
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
     
     
     
-    
-    
-
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -16,17 +16,19 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-   /* override func viewDidAppear(_ animated: Bool) {
+
+    override func viewDidAppear(_ animated: Bool) {
         if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
-            self.performSegue(withIdentifier: "loginHome", sender: self)
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
         }
-    }*/
+    }
     
     
     @IBAction func onLoginButton(_ sender: Any){
         
         let myUrl = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Could not log in!")
